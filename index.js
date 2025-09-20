759a602e391dd94b174286cbc3800a50e3bf0f33
yash-ladda <yashladda7777@gmail.com>
2025-09-20 18:38:58 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const port = 4000
+
+
+app.listen(provess.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World, this is home route')
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1>Please login at our site</h1>')
+})
+
+app.get('/githubname', (req, res)=>{
+    res.send('<a href="https://github.com/yash-ladda"> Click here to go on github profile</a>')
+})
+
---
+++ readme.txt
@@ -0,0 +1,2 @@
+1. to initialize node project write command: npm init
+2. in package.json write "start" as a alternative for node index.js. and whenever we want to run index.js write "npm run start"
